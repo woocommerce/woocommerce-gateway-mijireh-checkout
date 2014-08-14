4568e5f1c8b5fe95ecba3e54f2670058fd9ebc1c
claudiosmweb <contato@claudiosmweb.com>
2014-08-14 16:02:44 -0300

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,148 @@
+/* jshint node:true */
+module.exports = function( grunt ) {
+	'use strict';
+
+	grunt.initConfig({
+
+		// Package vars
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		// Setting folder templates
+		dirs: {
+			css:    'assets/css',
+			fonts:  'assets/fonts',
+			images: 'assets/images',
+			js:     'assets/js'
+		},
+
+		// Javascript linting with jshint
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= dirs.js %>/*.js',
+				'!<%= dirs.js %>/*.min.js'
+			]
+		},
+
+		// Minify .js files.
+		uglify: {
+			options: {
+				preserveComments: 'some'
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.js %>/',
+					src: [
+						'*.js',
+						'!*.min.js'
+					],
+					dest: '<%= dirs.js %>/',
+					ext: '.min.js'
+				}]
+			}
+		},
+
+		// Minify all .css files.
+		cssmin: {
+			dist: {
+				expand: true,
+				cwd: '<%= dirs.css %>/',
+				src: [
+					'*.css',
+					'!*.min.css'
+				],
+				dest: '<%= dirs.css %>/',
+				ext: '.min.css'
+			}
+		},
+
+		// Watch changes for assets
+		watch: {
+			css: {
+				files: [
+					'<%= dirs.css %>/*.css',
+					'!<%= dirs.css %>/*.min.css'
+				],
+				tasks: ['cssmin']
+			},
+			js: {
+				files: [
+					'<%= dirs.js %>/*js',
+					'!<%= dirs.js %>/*.min.js'
+				],
+				tasks: ['jshint', 'uglify']
+			}
+		},
+
+		// Create pot files
+		makepot: {
+			dist: {
+				options: {
+					type: 'wp-plugin',
+					potHeaders: {
+						'report-msgid-bugs-to': 'https://github.com/woothemes/woocommerce-gateway-mijireh-checkout/issues',
+						'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+					}
+				}
+			}
+		},
+
+		// Check the plugin textdomain
+		checktextdomain: {
+			options:{
+				text_domain: '<%= pkg.name %>',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php', // Include all files
+					'!node_modules/**' // Exclude node_modules/
+				],
+				expand: true
+			}
+		},
+
+		// Convert po files to mo files
+		po2mo: {
+			files: {
+				src: 'languages/*.po',
+				expand: true
+			}
+		}
+	});
+
+	// Load NPM tasks to be used here
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks( 'grunt-po2mo' );
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'cssmin',
+		'jshint',
+		'uglify'
+	]);
+};
